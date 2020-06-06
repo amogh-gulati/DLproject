1ad03c6543add7c734c6bc715a131edb8d415ffa
amogh <amogh7777@gmail.com>
2020-06-06 11:55:05 +0530

    aja results aja pls bhajwe aja

--- train.py
+++ train.py
@@ -70,14 +70,14 @@ def main(args=None):
     else:
         raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
 
-    sampler = AspectRatioBasedSampler(dataset_train, batch_size=8, drop_last=False)
+    sampler = AspectRatioBasedSampler(dataset_train, batch_size=16, drop_last=False)
     dataloader_train = DataLoader(dataset_train, num_workers=3, collate_fn=collater, batch_sampler=sampler)
 
     if dataset_val is not None:
-        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=8, drop_last=False)
+        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=16, drop_last=False)
         dataloader_val = DataLoader(dataset_val, num_workers=3, collate_fn=collater, batch_sampler=sampler_val)
 
-        sampler_test = AspectRatioBasedSampler(dataset_test, batch_size=8, drop_last=False)
+        sampler_test = AspectRatioBasedSampler(dataset_test, batch_size=16, drop_last=False)
         dataloader_test = DataLoader(dataset_val, num_workers=3, collate_fn=collater, batch_sampler=sampler_test)
 
     # Create the model
@@ -86,7 +86,8 @@ def main(args=None):
     elif parser.depth == 34:
         retinanet = model.resnet34(num_classes=dataset_train.num_classes(), pretrained=True)
     elif parser.depth == 50:
-        retinanet = model.resnet50(num_classes=dataset_train.num_classes(), pretrained=True)
+        retinanet = model.resnet50(num_classes=dataset_train.num_classes(), pretrained=False)
+        retinanet = torch.load("csv_retinanet_2_regular.pt")
     elif parser.depth == 101:
         retinanet = model.resnet101(num_classes=dataset_train.num_classes(), pretrained=True)
     elif parser.depth == 152:
@@ -111,28 +112,28 @@ def main(args=None):
 
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
 
-    
+   
 
-    
+   
 
     print('Num training images: {}'.format(len(dataset_train)))
 
     for epoch_num in range(parser.epochs):
-
+        '''
         loss_hist = collections.deque(maxlen=500)
-        
-        
+       
+       
         retinanet.train()
         retinanet.module.freeze_bn()
-        
+       
         epoch_loss = []
         c_loss = []
         r_loss = []
-        
+       
         for iter_num, data in enumerate(dataloader_train):
             try:
                 optimizer.zero_grad()
-                
+               
                 if torch.cuda.is_available():
                     classification_loss, regression_loss = retinanet([data['img'].cuda().float(), data['annot']])
 
@@ -170,7 +171,7 @@ def main(args=None):
                 continue
 
 
-        
+       
 
 
         gf=open('log.txt','a')
@@ -180,8 +181,8 @@ def main(args=None):
         #gf.write(str(mAP)+" ")
         #gf.write('\n')
         gf.close()
-		
-        
+
+       
 
 
         epoch_loss = []
@@ -191,25 +192,25 @@ def main(args=None):
             #retinanet.eval()
             optimizer.zero_grad()
             if torch.cuda.is_available():
-                
+               
                 classification_loss, regression_loss = retinanet([data['img'].cuda().float(), data['annot']])
 
             else:
-                
+               
                 classification_loss, regression_loss = retinanet([data['img'].float(), data['annot']])
                    
             classification_loss = classification_loss.mean()
             regression_loss = regression_loss.mean()
 
             loss = classification_loss + regression_loss
-            
+           
 
             if bool(loss == 0):
                 continue
 
-            
+           
             loss.backward()
-                
+               
 
             epoch_loss.append(float(loss))
 
@@ -221,8 +222,8 @@ def main(args=None):
 
             del classification_loss
             del regression_loss
-            
-
+           
+        '''
 
         retinanet.eval()
         if parser.dataset == 'coco':
@@ -238,9 +239,11 @@ def main(args=None):
             mAP = csv_eval.evaluate(dataset_val, retinanet)
 
 
-            mAP2 = csv_eval.evaluate(dataset_test, retinanet)
+            #mAP2 = csv_eval.evaluate(dataset_test, retinanet)
 
+        print("Validation MAP with pranav model is ", mAP)
 
+        '''
         gf=open('log.txt','a')
         gf.write(str(np.mean(c_loss))+" ")
         gf.write(str(np.mean(r_loss))+" ")
@@ -248,13 +251,13 @@ def main(args=None):
         gf.write(str(mAP)+" ")
         gf.write(str(mAP2)+" "+"\n")
         gf.close()
-
-
+        '''
+        print(1/0)
 
 
         scheduler.step(np.mean(epoch_loss))
 
-        
+       
         torch.save(retinanet.module, '{}_retinanet_{}.pt'.format(parser.dataset, epoch_num))
 
     retinanet.eval()
